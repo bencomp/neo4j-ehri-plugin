fb1bff88a2ecf341fdf1889f490f85114bab7d0b
bencomp <ben@companjen.name>
2013-09-19 17:22:59 +0200

    Refactor SKOS import to use Jena (far from done)

--- ehri-importers/src/main/java/eu/ehri/project/importers/cvoc/SkosRDFHandler.java
+++ ehri-importers/src/main/java/eu/ehri/project/importers/cvoc/SkosRDFHandler.java
@@ -1,6 +1,5 @@
 package eu.ehri.project.importers.cvoc;
 
-import org.apache.jena.riot.RDFDataMgr;
 import org.slf4j.LoggerFactory;
 
 import com.hp.hpl.jena.rdf.model.Model;
@@ -9,29 +8,33 @@ import com.hp.hpl.jena.rdf.model.ResIterator;
 import com.hp.hpl.jena.rdf.model.Resource;
 import com.hp.hpl.jena.rdf.model.ResourceFactory;
 import com.hp.hpl.jena.rdf.model.SimpleSelector;
-import com.hp.hpl.jena.util.FileManager;
 import com.hp.hpl.jena.vocabulary.RDF;
 
 /*
  * Reads an RDF file containing a SKOS thesaurus and creates required stuff for ingest in the graph database.
  * @author benc
  */
-public class SkosRDFImporter {
-	private static final org.slf4j.Logger logger = LoggerFactory.getLogger(SkosRDFImporter.class);
-//	private RDF fm;
+public class SkosRDFHandler {
+	private static final org.slf4j.Logger logger = LoggerFactory.getLogger(SkosRDFHandler.class);
 	private Model model;
+	private SkosRDFImporter importer;
+	
+	private final Resource SKOS_CONCEPT = ResourceFactory.createResource("http://www.w3.org/2004/02/skos/core#Concept");
+	private final Resource SKOS_CONCEPTSCHEME = ResourceFactory.createResource("http://www.w3.org/2004/02/skos/core#ConceptScheme");
 	
 	/*
 	 * Constructor. Loads an RDF file in the internal model.
 	 * @param filename
 	 */
-	public SkosRDFImporter(String filename) {
-		logger.debug("start of constructor");
-//		this.fm = FileManager.get();
-//		this.model = RDFDataMgr.loadModel(filename);
+	public SkosRDFHandler(String filename) {
+		logger.debug("Create an empty Model");
 		this.model = ModelFactory.createDefaultModel() ;
 		this.model.read(filename);
 		logger.debug("end of constructor");
+	}
+	
+	public SkosRDFHandler(String filename, SkosRDFImporter importer) {
+		this(filename);
 	}
 	
 	public void closeModel(){
@@ -47,7 +50,7 @@ public class SkosRDFImporter {
 	 */
 	public long getSubjectCount() {
 		logger.debug("start of subject count");
-		Model nmodel = this.model.query(new SimpleSelector(null, RDF.type, ResourceFactory.createResource("http://www.w3.org/2004/02/skos/core#Concept")));
+		Model nmodel = this.model.query(new SimpleSelector(null, RDF.type, SKOS_CONCEPT));
 		logger.debug("end of subject count");
 		return nmodel.size();
 	}
@@ -64,7 +67,24 @@ public class SkosRDFImporter {
 //			logger.debug("s: " + r);
 			System.out.println("s: "+r);
 		}
-//		logger.debug("listing subjects");
 		logger.debug("end of concept listing");
+	}
+	
+	/*
+	 * Create the Concepts
+	 */
+	public void createConcepts() {
+		
+	}
+	
+	/*
+	 * Create a Vocabulary bundle
+	 */
+	public void createVocabulary() {
+		// Find the ConceptScheme
+		ResIterator voc = model.listResourcesWithProperty(RDF.type, SKOS_CONCEPTSCHEME);
+		for(Resource res : voc.toList()) {
+			// 
+		}
 	}
 }
--- ehri-importers/src/test/java/eu/ehri/project/importers/cvoc/SkosRDFImporterTest.java
+++ ehri-importers/src/test/java/eu/ehri/project/importers/cvoc/SkosRDFImporterTest.java
@@ -5,23 +5,85 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 import org.slf4j.LoggerFactory;
 
+import eu.ehri.project.definitions.Ontology;
 import eu.ehri.project.importers.AbstractImporterTest;
+import eu.ehri.project.models.base.Description;
+import eu.ehri.project.models.cvoc.Concept;
+import eu.ehri.project.models.cvoc.ConceptDescription;
+import eu.ehri.project.models.cvoc.Vocabulary;
 
+/*
+ * Test loading SKOS vocabulary/-ies using an RDF library.
+ * @author benc
+ */
 public class SkosRDFImporterTest extends AbstractImporterTest {
 	
 	private static final org.slf4j.Logger logger = LoggerFactory.getLogger(SkosRDFImporterTest.class);
     private final String FILENAME = "ehri-skos.rdf";
+    
+    private final String CONCEPT_URI_1 = "http://ehri01.dans.knaw.nl/tematres/vocab/?tema=511";
+    private final String CONCEPT_URI_2 = "http://ehri01.dans.knaw.nl/tematres/vocab/?tema=512";
+    private final String CONCEPTSCHEME_URI = "http://ehri01.dans.knaw.nl/tematres/vocab/";
 
 	@Test
-	public void testListConcepts() throws Exception {
-		logger.debug("start of test");
-		SkosRDFImporter importer = new SkosRDFImporter(FILENAME);
+	public void testReadConcepts() throws Exception {
+		logger.debug("start read concepts test");
+		SkosRDFHandler handler = new SkosRDFHandler(FILENAME);
 		
 		logger.debug("testing subject count");
-		assertEquals(881L, importer.getSubjectCount());
-		assertEquals(7217L, importer.getStatementCount());
-//		importer.listConcepts();
-		importer.closeModel();
+		assertEquals(881L, handler.getSubjectCount());
+		assertEquals(7217L, handler.getStatementCount());
+		handler.closeModel();
+	}
+	
+	/*
+	 * Load statements and check that the concepts were loaded into Neo4J correctly, and linked correctly.
+	 */
+	@Test
+	public void testConceptLinks() throws Exception {
+        final String logMessage = "Importing a SKOS file";
+		logger.debug("start concept links test");
+		
+		int beforeNodeCount = getNodeCount(graph);
+        
+		SkosRDFHandler handler = new SkosRDFHandler(FILENAME);
+		// LOAD HERE
+		
+		int afterNodeCount = getNodeCount(graph);
+		/**
+         * We should have afterwards:
+         *  - 881 new concepts
+         *  - 1386 new descriptions (1 per prefLabel)
+         *  - 1 new Event
+         *  - 882 new event links (1 for user, 1 per concept)
+         */
+        assertEquals(beforeNodeCount + 881 + 1386 + 1 + 882, afterNodeCount);
+
+		
+		
+		Vocabulary scheme = (Vocabulary) graph.getVertex(CONCEPTSCHEME_URI);
+		
+		// Does Concept with URI <http://ehri01.dans.knaw.nl/tematres/vocab/?tema=511>
+		// 	have a description with English prefLabel "Prisoners of war"?
+		Concept con = (Concept) graph.getVertex(CONCEPT_URI_1);
+		
+		for(Description des : con.getDescriptions()) {
+			if (des.getLanguageOfDescription().equals("en")) {
+				assertEquals(des.asVertex().getProperty(Ontology.NAME_KEY), "Prisoners of war");
+				assertEquals(des.asVertex().getProperty(Ontology.CONCEPT_ALTLABEL), "POWs");
+			} else if (des.getLanguageOfDescription().equals("de")) {
+				assertEquals(des.asVertex().getProperty(Ontology.NAME_KEY), "Kriegsgefangener");
+				assertEquals(des.asVertex().getProperty(Ontology.CONCEPT_ALTLABEL), "Kriegsgefangene");
+			}
+		}
+		
+		for(Concept narrower : con.getNarrowerConcepts()) {
+			assertEquals(narrower, (Concept) graph.getVertex(CONCEPT_URI_2));
+		}
+		
+		// Does `con` have a link to the `scheme` Vocabulary? 
+		assertEquals(scheme, con.getVocabulary());
+		
 	}
 
 }
